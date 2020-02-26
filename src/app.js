fd794343b6f433d8ecc2e6fafdd9787d117ad2f1
AstroKD <kdreyerinseattle@gmail.com>
2020-02-26 15:43:06 -0800

    routes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,32 @@
 // 3rd party resources
-const path = require('path')
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express()
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+const authRouter = require('./routes/authRouter');
+app.use(authRouter);
+
+// Error catching
+const notFound = require('./middleware/notFound');
+app.use(notFound);
+const errorHandler = require('./middleware/errorHandler');
+app.use(errorHandler);
+
+//Export 
+module.exports = {
+  server: app,
+  start: port => {
+    app.listen(port, () => {
+      console.log(`Express server listening on port ${port}.`);
+    })
+  }
+}
